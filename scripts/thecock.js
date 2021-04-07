fd57f0283000d80766354682a02bece765070d2b
codfury <shivam04252@gmail.com>
2021-04-07 11:27:03 +0530

    commit two

--- scripts/thecock.js
+++ scripts/thecock.js
@@ -3,7 +3,7 @@ function theclock(){
     var h = rtc.getHours();
     var min = rtc.getMinutes();
     var sec = rtc.getSeconds();
-    var d = rtc.getDay();
+    var ddd = rtc.getDay();
     var mm  = rtc.getMonth();
     var yy = rtc.getFullYear();
     
@@ -13,13 +13,13 @@ function theclock(){
     h= ("0" + h).slice(-2); 
     min= ("0" + min).slice(-2); 
     sec = ("0" + sec).slice(-2);
-    d= ("0" + d).slice(-2); 
-    mm= ("0" + mm).slice(-2); 
+    ddd= ("0" + ddd).slice(-2); 
+    mmm= ("0" + mm).slice(-2); 
     yy = ("0" + yy).slice(-4);
     
     document.getElementById('clock').innerHTML =
-      h + "  :  " + min + "  :  " + sec + " " + ampm + "\n" +d + "/" + mm + "/" + yy;
+      //h + "  :  " + min + "  :  " + sec + " " + ampm + "\n" +d + "/" + mm + "/" + yy;
+      rtc;
       var t = setTimeout(theclock, 200);
-    
 
 }
